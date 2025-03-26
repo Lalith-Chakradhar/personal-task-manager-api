dcfd6557ee36f913aeed29915820d52c3be0d145
Lalith Chakradhar <lalith.rangana.31.3@mountblue.tech>
2025-03-26 13:01:48 +0530

    Implemented swagger documentation in auth routes, yet to implement it in tasks routes

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ import express from "express";
 import routes from "./routes/index.js";
 import errorHandler from "./middlewares/error.js";
 import config from "./config/config.js";
-
 import morganLogger from "morgan";
+
+import swaggerjsdoc from "swagger-jsdoc";
+import swaggerui from "swagger-ui-express";
 
 const app = express();
 
@@ -14,6 +16,32 @@ app.use(morganLogger("dev"));
 
 //Use all the routes
 app.use("/", routes);
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Personal Task Manager API docs",
+      version: "1.0.0",
+      description:
+      "A server containing APIs used for managing users and tasks, and documented with Swagger",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000/",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],  // Look for API documentation comments in all route files
+};
+
+const swaggerSpec = swaggerjsdoc(options);
+
+app.use(
+  "/api-docs",
+  swaggerui.serve,
+  swaggerui.setup(swaggerSpec),
+);
 
 //Use the errorHandler after the routes such that on doing a next call,
 //the errorHandler middleware is called
